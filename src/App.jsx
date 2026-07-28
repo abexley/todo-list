7c1e505dae296047c58eaccd687bb788a2a268ce
Your Name <youremail@example.com>
2026-07-27 22:41:56 -0700

    Complete lesson 4 Coding Assignment

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,20 @@ import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import "./App.css";
 
-const todos = [
-  { id: 1, title: "review resources" },
-  { id: 2, title: "take notes" },
-  { id: 3, title: "code out app" },
-];
-
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
 
+  function addTodo(todoTitle) {
+    const newTodo = {
+      id: Date.now(),
+      title: todoTitle,
+    };
+    setTodoList((previous) => [newTodo, ...previous]);
+  }
   return (
     <>
       <h1>Todo List</h1>
-      <TodoForm />
+      <TodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
     </>
   );
